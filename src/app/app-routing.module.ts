72a462a7f47d774c21ba5f7e2da185e57236118f
Piyush969182 <102027056+Piyush969182@users.noreply.github.com>
2022-04-13 21:18:22 +0530

    Structure added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './presentation/modules/login/login.component';
+import { SignupComponent } from './presentation/modules/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'signup',
+    component:SignupComponent
+  },
+  {
+    path:'home',
+    loadChildren:()=>import('./presentation/modules/home/home.module').then(m=>m.HomeModule)
+  },
+  {
+    path:'users',
+    loadChildren:()=>import('./presentation/modules/users/users.module').then(m=>m.UsersModule)
+  },
+  {
+    path:'books',
+    loadChildren:()=>import('./presentation/modules/books/books.module').then(m=>m.BooksModule)
+  },
+  {
+    path:'categories',
+    loadChildren:()=>import('./presentation/modules/categories/categories.module').then(m=>m.CategoriesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './presentation/modules/login/login.component';
+import { SignupComponent } from './presentation/modules/signup/signup.component';
+import { HomeComponent } from './presentation/modules/home/home.component';
+import { UsersComponent } from './presentation/modules/users/users.component';
+import { UserlistComponent } from './presentation/modules/users/userlist/userlist.component';
+import { AdduserComponent } from './presentation/modules/users/adduser/adduser.component';
+import { EdituserComponent } from './presentation/modules/users/edituser/edituser.component';
+import { BooksComponent } from './presentation/modules/books/books.component';
+import { CategoriesComponent } from './presentation/modules/categories/categories.component';
+import { BooklistComponent } from './presentation/modules/books/booklist/booklist.component';
+import { AddbookComponent } from './presentation/modules/books/addbook/addbook.component';
+import { EditbookComponent } from './presentation/modules/books/editbook/editbook.component';
+import { CategorylistComponent } from './presentation/modules/categories/categorylist/categorylist.component';
+import { AddcategoryComponent } from './presentation/modules/categories/addcategory/addcategory.component';
+import { EditcategoryComponent } from './presentation/modules/categories/editcategory/editcategory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    UsersComponent,
+    UserlistComponent,
+    AdduserComponent,
+    EdituserComponent,
+    BooksComponent,
+    CategoriesComponent,
+    BooklistComponent,
+    AddbookComponent,
+    EditbookComponent,
+    CategorylistComponent,
+    AddcategoryComponent,
+    EditcategoryComponent
   ],
   imports: [
     BrowserModule,
